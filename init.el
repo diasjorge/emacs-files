639b67fc6d142d6d7122896438fe0923e56ebfac
Jorge Dias <jorge@mrdias.com>
2009-12-18 17:21:06 +0100

    Changes to the autosaves and backup files

--- init.el
+++ init.el
@@ -78,25 +78,11 @@
                                   (revert-buffer-function " %b"
                                   ("%b - Dir:  " default-directory)))))))
 
-;; Put autosave files (ie #foo#) in one place, *not*
-;; scattered all over the file system!
-(defvar autosave-dir
-  (concat "/tmp/emacs_autosaves/" (user-login-name) "/"))
-(make-directory autosave-dir t)
-(setq auto-save-file-name-transforms `(("\\(?:[^/]*/\\)*\\(.*\\)" ,(concat autosave-dir "\\1") t)))
-
-(defun make-auto-save-file-name ()
-  (concat autosave-dir
-   (if buffer-file-name
-      (concat "#" (file-name-nondirectory buffer-file-name) "#")
-    (expand-file-name
-     (concat "#%" (buffer-name) "#")))))
-
-;; Put backup files (ie foo~) in one place too. (The backup-directory-alist
-;; list contains regexp=>directory mappings; filenames matching a regexp are
-;; backed up in the corresponding directory. Emacs will mkdir it if necessary.)
-(defvar backup-dir (concat "/tmp/emacs_backups/" (user-login-name) "/"))
-(setq backup-directory-alist (list (cons "." backup-dir)))
+;; Puts backup and autosave files in one place
+(setq backup-directory-alist
+      `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms
+      `((".*" ,temporary-file-directory t)))
 
 ;; disable menu bar
 (if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
