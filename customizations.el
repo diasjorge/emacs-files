6db366a7f886bf94d9b13bd7a066900e981180b3
Jorge Dias <jorge@mrdias.com>
2010-01-08 12:13:21 +0100

    added mumamo customizations

---
+++ customizations.el
@@ -0,0 +1,7 @@
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) nil)))
+ '(mumamo-background-chunk-submode ((((class color) (min-colors 88) (background dark)) (:background "gray22")))))
