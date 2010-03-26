d9e0b83fab3978312acda2699662fff2a217bf90
Jorge Dias <jorge@mrdias.com>
2010-03-26 10:37:59 +0100

    Adding support for autocomplete mode

--- init.el
+++ init.el
@@ -287,6 +287,13 @@
   (interactive)
   (insert "<!-- -**-END-**- -->"))
 
+;; auto-complete support
+(add-to-list 'load-path "~/.emacs.d/elisp/auto-complete")
+(require 'auto-complete-config)
+(add-to-list 'ac-dictionary-directories "~/.emacs.d/elisp/auto-complete/ac-dict")
+(ac-config-default)
+
+
 ;;;;;;;;;;;;;;;;;;;;
 ;; CUSTOMIZATIONS ;;
 ;;;;;;;;;;;;;;;;;;;;
