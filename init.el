e615b40410dbc7eb740b1eb11b4d05d40930956d
Jorge Dias <jorge@mrdias.com>
2010-09-15 17:55:56 +0200

    Open new line copied from vim. XD

--- init.el
+++ init.el
@@ -366,6 +366,35 @@
 ;; MY FUNCTIONS ;;
 ;;;;;;;;;;;;;;;;;;
 
+;; Behave like vi's o command
+(defun open-next-line (arg)
+  "Move to the next line and then opens a line.
+    See also `newline-and-indent'."
+  (interactive "p")
+  (end-of-line)
+  (open-line arg)
+  (next-line 1)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "M-y") 'open-next-line)
+
+;; Behave like vi's O command
+(defun open-previous-line (arg)
+  "Open a new line before the current one.
+     See also `newline-and-indent'."
+  (interactive "p")
+  (beginning-of-line)
+  (open-line arg)
+  (when newline-and-indent
+    (indent-according-to-mode)))
+
+(global-set-key (kbd "M-Y") 'open-previous-line)
+
+;; Autoindent open-*-lines
+(defvar newline-and-indent t
+  "Modify the behavior of the open-*-line functions to cause them to autoindent.")
+
 (defun untabify-buffer ()
   "Untabify current buffer"
   (interactive)
