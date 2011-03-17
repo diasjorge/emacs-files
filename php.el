4f32384c1b04fde307fb7098b550ae934bc2ccce
Jorge Dias <jorge.dias@ideup.com>
2011-03-17 15:54:16 +0100

    Drupal support improved

--- php.el
+++ php.el
@@ -4,3 +4,7 @@
 (add-to-list 'auto-mode-alist '("\\.\\(module\\|test\\|install\\|theme\\)$" . drupal-mode))
 (add-to-list 'auto-mode-alist '("\\.\\(php\\|inc\\)$" . php-mode))
 (add-to-list 'auto-mode-alist '("\\.info" . conf-windows-mode))
+(add-to-list 'auto-mode-alist '("sites\\/all\\/.*\\.\\(php\\|inc\\)$" . drupal-mode))
+
+
+
