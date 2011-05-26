965dca23909311cfea098319a4a0e2571dd9a635
Jorge Dias <jorge@mrdias.com>
2011-05-26 17:53:42 +0200

    Updated customizations

--- customizations.el
+++ customizations.el
@@ -4,4 +4,12 @@
   ;; Your init file should contain only one such instance.
   ;; If there is more than one, they won't work right.
  '(mumamo-background-chunk-major ((((class color) (min-colors 88) (background dark)) nil)))
- '(mumamo-background-chunk-submode ((((class color) (min-colors 88) (background dark)) (:background "gray22")))))
+ '(mumamo-background-chunk-submode1 ((((class color) (min-colors 88) (background dark)) (:background "gray22")))))
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(show-paren-mode t)
+ '(tool-bar-mode nil))
