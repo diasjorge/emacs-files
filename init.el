e07fe928ba539ba3366b1870bd1ef1ee3d189c85
Jorge Dias <jorge@mrdias.com>
2011-10-07 16:49:35 +0200

    Load mac preferences before packages

--- init.el
+++ init.el
@@ -3,13 +3,13 @@
 (add-to-list 'load-path (file-name-directory (or load-file-name buffer-file-name)))
 
 (load "init_pre_init")
-(load "init_packages")
-(load "init_preferences")
-(load "init_keybindings")
 
 (when (string-equal system-type "darwin")
   (load "init_mac"))
 
+(load "init_packages")
+(load "init_preferences")
+(load "init_keybindings")
 (load "custom_functions")
 (load "environment" t)
 
