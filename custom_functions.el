d78d0413daa61d4eea021805a08ad53da98ad9e8
Jorge Dias <jorge@mrdias.com>
2012-01-02 18:21:00 +0100

    Added function to rename file and buffer

--- custom_functions.el
+++ custom_functions.el
@@ -137,3 +137,20 @@
   (interactive "r")
   (let ((comando "css2sass -s"))
   (shell-command-on-region beg end comando (buffer-name) t)))
+
+(defun rename-this-buffer-and-file ()
+  "Renames current buffer and file it is visiting."
+  (interactive)
+  (let ((name (buffer-name))
+        (filename (buffer-file-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+      (let ((new-name (read-file-name "New name: " (file-name-directory filename))))
+        (cond ((get-buffer new-name)
+               (error "A buffer named '%s' already exists!" new-name))
+              (t
+               (rename-file filename new-name 1)
+               (rename-buffer new-name)
+               (set-visited-file-name new-name)
+               (set-buffer-modified-p nil)
+               (message "File '%s' successfully renamed to '%s'" name (file-name-nondirectory new-name))))))))
