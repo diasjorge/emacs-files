4222c61bfc888e366fa34fb42f1bb36a3c881621
Jorge Dias <jorge@mrdias.com>
2012-01-02 18:21:42 +0100

    Added new preferences
    Saving current session
    Saving minibuffer history

--- init_preferences.el
+++ init_preferences.el
@@ -44,9 +44,19 @@
                                   ("%b - Dir:  " default-directory)))))))
 
 ;; No backup or auto-save
+(setq backup-by-copying t)
 (setq make-backup-files nil)
 (setq auto-save-default nil)
-(setq backup-by-copying t)
+
+;; Save minibuffer history
+(savehist-mode 1)
+
+;; Make lines not dissapear into the right margin while in org-mode
+(add-hook 'org-mode-hook 'soft-wrap-lines)
+
+;; turn on save place so that when opening a file, the cursor will be at the last position.
+(require 'saveplace)
+(setq-default save-place t)
 
 ;; Delete to trash
 (setq delete-by-moving-to-trash t)
@@ -68,6 +78,7 @@
 
 ;; Paren mode globally
 (show-paren-mode 1)
+(setq show-paren-style 'expression)
 
 ;; I hate tabs!
 (setq-default indent-tabs-mode nil)
@@ -112,3 +123,99 @@
 ;; preview command completion when writing in Minibuffer
 ;; this is part of emacs
 (icomplete-mode 1)
+
+;; display pressed keys faster
+(setq echo-keystrokes 0.02)
+
+;; § ----------------------------------------
+;; auto compile elisp files after save, do so only if there's exists a byte-compiled file
+(defun auto-recompile-el-buffer ()
+  (interactive)
+  (when (and (eq major-mode 'emacs-lisp-mode)
+             (file-exists-p (byte-compile-dest-file buffer-file-name)))
+    (byte-compile-file buffer-file-name)))
+
+(add-hook 'after-save-hook 'auto-recompile-el-buffer)
+
+;; § ----------------------------------------
+;; Make emacs open all files in last emacs session.
+;;
+;; This functionality is provided by desktop-save-mode (“feature”
+;; name: “desktop”). The mode is not on by default in emacs 23.1, and
+;; has a lot options. The following is init settings for the mode for
+;; ErgoEmacs.
+;;
+;; Goal: have emacs always auto open the set of opend files in last
+;; session, even if emacs crashed in last session or the OS crashed in
+;; last session. Also, don't bother users by asking questions like “do
+;; you want to save desktop?” or “do you want to override last session
+;; file?”, because these are annoying and terms like “session” or
+;; “desktop” are confusing to most users because it can have many
+;; meanings.
+
+;; Some tech detail: set the desktop session file 〔.emacs.desktop〕 at the variable
+;; “user-emacs-directory” (default value is “~/.emacs.d/”).  This file
+;; is our desktop file. It will be auto created and or over-written.
+;; if a emacs expert has other desktop session files elsewhere, he can
+;; still use or manage those.
+
+(require 'desktop)
+
+(defun desktop-settings-setup ()
+  "Some settings setup for desktop-save-mode."
+  (interactive)
+
+  ;; At this point the desktop.el hook in after-init-hook was
+  ;; executed, so (desktop-read) is avoided.
+  (when (not (eq (emacs-pid) (desktop-owner))) ; Check that emacs did not load a desktop yet
+    ;; Here we activate the desktop mode
+    (desktop-save-mode 1)
+
+    ;; The default desktop is saved always
+    (setq desktop-save t)
+
+    ;; The default desktop is loaded anyway if it is locked
+    (setq desktop-load-locked-desktop t)
+
+    ;; Set the location to save/load default desktop
+    (setq desktop-dirname user-emacs-directory)
+
+    ;; Make sure that even if emacs or OS crashed, emacs
+    ;; still have last opened files.
+    (add-hook 'find-file-hook
+     (lambda ()
+       (run-with-timer 5 nil
+          (lambda ()
+            ;; Reset desktop modification time so the user is not bothered
+            (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))
+            (desktop-save user-emacs-directory)))))
+
+    ;; Read default desktop
+    (if (file-exists-p (concat desktop-dirname desktop-base-file-name))
+        (desktop-read desktop-dirname))
+
+    ;; Add a hook when emacs is closed to we reset the desktop
+    ;; modification time (in this way the user does not get a warning
+    ;; message about desktop modifications)
+    (add-hook 'kill-emacs-hook
+              (lambda ()
+                ;; Reset desktop modification time so the user is not bothered
+                (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))))
+    )
+  )
+
+(add-hook 'after-init-hook
+          'desktop-settings-setup
+          (lambda ()
+            ;; No splash screen
+            (setq inhibit-startup-screen t)
+
+            ;; If the *scratch* buffer is the current one, then create a new
+            ;; empty untitled buffer to hide *scratch*
+            (if (string= (buffer-name) "*scratch*")
+                (new-empty-buffer))
+            )
+          t) ;; append this hook to the tail
+
+
+;; § ----------------------------------------
