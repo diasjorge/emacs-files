a48fa851a72e74ca5070aa3fc8b4429790fda068
Jorge Dias <jorge@mrdias.com>
2013-04-11 17:27:46 +0200

    Fixes bug moving text in emacs 24.3

--- custom_functions.el
+++ custom_functions.el
@@ -274,25 +274,29 @@
       (delete-trailing-whitespace))))
 
 (defun move-text-internal (arg)
-   (cond
-    ((and mark-active transient-mark-mode)
-     (if (> (point) (mark))
-            (exchange-point-and-mark))
-     (let ((column (current-column))
-              (text (delete-and-extract-region (point) (mark))))
-       (forward-line arg)
-       (move-to-column column t)
-       (set-mark (point))
-       (insert text)
-       (exchange-point-and-mark)
-       (setq deactivate-mark nil)))
-    (t
-     (beginning-of-line)
-     (when (or (> arg 0) (not (bobp)))
-       (forward-line)
-       (when (or (< arg 0) (not (eobp)))
-            (transpose-lines arg))
-       (forward-line -1)))))
+  (cond
+   ((and mark-active transient-mark-mode)
+    (if (> (point) (mark))
+        (exchange-point-and-mark))
+    (let ((column (current-column))
+          (text (delete-and-extract-region (point) (mark))))
+      (forward-line arg)
+      (move-to-column column t)
+      (set-mark (point))
+      (insert text)
+      (exchange-point-and-mark)
+      (setq deactivate-mark nil)))
+   (t
+    (let ((column (current-column)))
+      (beginning-of-line)
+      (when (or (> arg 0) (not (bobp)))
+        (forward-line )
+        (when (or (< arg 0) (not (eobp)))
+          (transpose-lines arg)
+          (when (and (string< "24.3" emacs-version) (< arg 0))
+            (forward-line -1)))         ;works  in emacs 24.3
+        (forward-line -1))
+      (move-to-column column t)))))
 
 (defun move-text-down (arg)
    "Move region (transient-mark-mode active) or current line
