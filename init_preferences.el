589325abf315011fa76c3135495bb4cbe0c26b1d
Jorge Dias <jorge@mrdias.com>
2013-06-10 10:46:02 +0200

    Create directory on save

--- init_preferences.el
+++ init_preferences.el
@@ -189,6 +189,17 @@
 
 (add-hook 'after-save-hook 'auto-recompile-el-buffer)
 
+;| Create intermediate directories when saving a new file in a non existing path.
+
+; http://stackoverflow.com/questions/6830671/how-to-make-emacs-create-intermediate-dirs-when-saving-a-file
+(add-hook 'before-save-hook
+  (lambda ()
+    (when buffer-file-name
+      (let ((dir (file-name-directory buffer-file-name)))
+        (when (and (not (file-exists-p dir))
+                   (y-or-n-p (format "Directory %s does not exist. Create it? " dir)))
+          (make-directory dir t))))))
+
 ;; § ----------------------------------------
 ;; Make emacs open all files in last emacs session.
 ;;
