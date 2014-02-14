c08123fb606b6344e9fab1aaffbeb2e012e31204
Jorge Dias <jorge@mrdias.com>
2014-02-14 18:03:24 +0100

    Extract ruby mode to init file

---
+++ lisp/el-get-init-files/init-ruby-mode.el
@@ -0,0 +1,46 @@
+(add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
+
+(add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.irbrc$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.builder$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.autotest$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.jbuilder$" . ruby-mode))
+
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (setq c-tab-always-indent nil)
+                             (ruby-fancy-indent)
+                             (if (and buffer-file-name (string-match-p "\\.erb" buffer-file-name))
+                                 (setq ruby-insert-encoding-magic-comment nil))))
+
+;; Ruby support for hs-mode
+(add-to-list 'hs-special-modes-alist
+             '(ruby-mode
+               "\\(def\\|do\\|{\\)" "\\(end\\|end\\|}\\)" "#"
+               (lambda (arg) (ruby-end-of-block)) nil))
+
+(add-hook 'ruby-mode-hook '(lambda ()
+                             (hs-minor-mode 1)))
+
+;; Remove after update to emacs 24.4
+(defadvice ruby-indent-line (after unindent-closing-paren activate)
+  (let ((column (current-column))
+        indent offset)
+    (save-excursion
+      (back-to-indentation)
+      (let ((state (syntax-ppss)))
+        (setq offset (- column (current-column)))
+        (when (and (eq (char-after) ?\))
+                   (not (zerop (car state))))
+          (goto-char (cadr state))
+          (setq indent (current-indentation)))))
+    (when indent
+      (indent-line-to indent)
+      (when (> offset 0) (forward-char offset)))))
--- lisp/init_packages.el
+++ lisp/init_packages.el
@@ -13,55 +13,7 @@
 (add-to-list 'load-path
              (concat emacs-directory "el-get/el-get"))
 
-(defun ruby-mode-after-load ()
-  (autoload 'ruby-mode "ruby-mode" nil t)
-  (add-to-list 'interpreter-mode-alist '("ruby" . ruby-mode))
-
-  (add-to-list 'auto-mode-alist '("\\.rb$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.ru$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Capfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("Guardfile" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.thor$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.irbrc$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.builder$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.autotest$" . ruby-mode))
-  (add-to-list 'auto-mode-alist '("\\.jbuilder$" . ruby-mode))
-
-  (add-hook 'ruby-mode-hook '(lambda ()
-                               (setq c-tab-always-indent nil)
-                               (ruby-fancy-indent)
-                               (if (and buffer-file-name (string-match-p "\\.erb" buffer-file-name))
-                                   (setq ruby-insert-encoding-magic-comment nil))))
-
-  ;; Ruby support for hs-mode
-  (add-to-list 'hs-special-modes-alist
-	     '(ruby-mode
-	       "\\(def\\|do\\|{\\)" "\\(end\\|end\\|}\\)" "#"
-	       (lambda (arg) (ruby-end-of-block)) nil))
-
-  (add-hook 'ruby-mode-hook '(lambda ()
-                               (hs-minor-mode 1)))
-
-  ;; Remove after update to emacs 24.4
-  (defadvice ruby-indent-line (after unindent-closing-paren activate)
-    (let ((column (current-column))
-          indent offset)
-      (save-excursion
-        (back-to-indentation)
-        (let ((state (syntax-ppss)))
-          (setq offset (- column (current-column)))
-          (when (and (eq (char-after) ?\))
-                     (not (zerop (car state))))
-            (goto-char (cadr state))
-            (setq indent (current-indentation)))))
-      (when indent
-        (indent-line-to indent)
-        (when (> offset 0) (forward-char offset)))))
-)
+(setq el-get-user-package-directory (concat emacs-directory "lisp/el-get-init-files"))
 
 (defun yaml-mode-after-load ()
   (autoload 'yaml-mode "yaml-mode" nil t)
@@ -306,8 +258,7 @@
         (:name rvm
                :after (progn (rvm-after-load)))
         (:name ruby-mode
-               :type builtin
-               :after (progn (ruby-mode-after-load)))
+               :type builtin)
         (:name inf-ruby
                :type elpa
                :after (progn (inf-ruby-after-load)))
