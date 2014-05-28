04341c90c957beca1322247c6c61a32b0b85919d
Jorge Dias <jorge@mrdias.com>
2014-05-28 12:48:00 +0200

    Remove customization for show-paren

--- lisp/customizations.el
+++ lisp/customizations.el
@@ -8,9 +8,3 @@
  '(ibuffer-default-sorting-mode (quote filename/process))
  '(markdown-command "bluecloth")
  '(safe-local-variable-values (quote ((ruby-test-rspec-executables "zeus-rspec") (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (ruby-test-default-library . "spec") (eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace (point-min) (point-max)) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t) (encoding . utf-8) (ruby-deep-indent-paren) (ruby-compilation-executable . "ruby") (ruby-compilation-executable . "ruby1.8") (ruby-compilation-executable . "ruby1.9") (ruby-compilation-executable . "rbx") (ruby-compilation-executable . "jruby")))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(show-paren-match ((t (:background "azure2" :weight normal)))))
