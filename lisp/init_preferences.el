cf77337e472838961aa9df091b265626107dcb2c
Jorge Dias <jorge@mrdias.com>
2014-07-25 11:37:01 +0200

    Remove desktop save code

--- lisp/init_preferences.el
+++ lisp/init_preferences.el
@@ -222,67 +222,67 @@
 ;; if a emacs expert has other desktop session files elsewhere, he can
 ;; still use or manage those.
 
-(require 'desktop)
+;; (require 'desktop)
 
-(defun desktop-settings-setup ()
-  "Some settings setup for desktop-save-mode."
-  (interactive)
+;; (defun desktop-settings-setup ()
+;;   "Some settings setup for desktop-save-mode."
+;;   (interactive)
 
-  ;; At this point the desktop.el hook in after-init-hook was
-  ;; executed, so (desktop-read) is avoided.
-  (when (not (eq (emacs-pid) (desktop-owner))) ; Check that emacs did not load a desktop yet
-    ;; Here we activate the desktop mode
-    ;; (desktop-save-mode 1)
+;;   ;; At this point the desktop.el hook in after-init-hook was
+;;   ;; executed, so (desktop-read) is avoided.
+;;   (when (not (eq (emacs-pid) (desktop-owner))) ; Check that emacs did not load a desktop yet
+;;     ;; Here we activate the desktop mode
+;;     ;; (desktop-save-mode 1)
 
-    ;; The default desktop is saved always
-    ;; (setq desktop-save t)
+;;     ;; The default desktop is saved always
+;;     ;; (setq desktop-save t)
 
-    ;; The default desktop is loaded anyway if it is locked
-    ;; (setq desktop-load-locked-desktop t)
+;;     ;; The default desktop is loaded anyway if it is locked
+;;     ;; (setq desktop-load-locked-desktop t)
 
-    ;; Set the location to save/load default desktop
-    (setq desktop-dirname user-emacs-directory)
+;;     ;; Set the location to save/load default desktop
+;;     (setq desktop-dirname user-emacs-directory)
 
-    ;; Make sure that even if emacs or OS crashed, emacs
-    ;; still have last opened files.
-    ;; (add-hook 'find-file-hook
-    ;;  (lambda ()
-    ;;    (run-with-timer 5 nil
-    ;;       (lambda ()
-    ;;         ;; Reset desktop modification time so the user is not bothered
-    ;;         (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))
-    ;;         (desktop-save user-emacs-directory)))))
+;;     ;; Make sure that even if emacs or OS crashed, emacs
+;;     ;; still have last opened files.
+;;     ;; (add-hook 'find-file-hook
+;;     ;;  (lambda ()
+;;     ;;    (run-with-timer 5 nil
+;;     ;;       (lambda ()
+;;     ;;         ;; Reset desktop modification time so the user is not bothered
+;;     ;;         (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))
+;;     ;;         (desktop-save user-emacs-directory)))))
 
-    ;; Read default desktop
-    (if (file-exists-p (concat desktop-dirname desktop-base-file-name))
-        (desktop-read desktop-dirname))
+;;     ;; Read default desktop
+;;     (if (file-exists-p (concat desktop-dirname desktop-base-file-name))
+;;         (desktop-read desktop-dirname))
 
-    ;; Add a hook when emacs is closed to we reset the desktop
-    ;; modification time (in this way the user does not get a warning
-    ;; message about desktop modifications)
-    (add-hook 'kill-emacs-hook
-              (lambda ()
-                ;; Reset desktop modification time so the user is not bothered
-                (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))))
-    )
-  )
+;;     ;; Add a hook when emacs is closed to we reset the desktop
+;;     ;; modification time (in this way the user does not get a warning
+;;     ;; message about desktop modifications)
+;;     (add-hook 'kill-emacs-hook
+;;               (lambda ()
+;;                 ;; Reset desktop modification time so the user is not bothered
+;;                 (setq desktop-file-modtime (nth 5 (file-attributes (desktop-full-file-name))))))
+;;     )
+;;   )
 
-(add-hook 'after-init-hook
-          'desktop-settings-setup
-          (lambda ()
-            ;; No splash screen
-            (setq inhibit-startup-screen t)
+;; (add-hook 'after-init-hook
+;;           'desktop-settings-setup
+;;           (lambda ()
+;;             ;; No splash screen
+;;             (setq inhibit-startup-screen t)
 
-            ;; If the *scratch* buffer is the current one, then create a new
-            ;; empty untitled buffer to hide *scratch*
-            (if (string= (buffer-name) "*scratch*")
-                (new-empty-buffer))
-            )
-          t) ;; append this hook to the tail
+;;             ;; If the *scratch* buffer is the current one, then create a new
+;;             ;; empty untitled buffer to hide *scratch*
+;;             (if (string= (buffer-name) "*scratch*")
+;;                 (new-empty-buffer))
+;;             )
+;;           t) ;; append this hook to the tail
 
-(add-hook 'desktop-after-read-hook
-          (lambda ()
-            (message "desktop read")
-            (desktop-remove)
-            (desktop-release-lock)))
-;; ;; § ----------------------------------------
+;; (add-hook 'desktop-after-read-hook
+;;           (lambda ()
+;;             (message "desktop read")
+;;             (desktop-remove)
+;;             (desktop-release-lock)))
+;; ;; ;; § ----------------------------------------
