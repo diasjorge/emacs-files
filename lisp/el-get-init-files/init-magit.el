b6da8a037cf5af18e677bd7ab83aa883b052656a
Jorge Dias <jorge.dias@schibsted.com>
2015-10-27 11:19:51 +0100

    Changes for magit newest version

--- lisp/el-get-init-files/init-magit.el
+++ lisp/el-get-init-files/init-magit.el
@@ -5,17 +5,17 @@
     (shell-command "git --no-pager commit --amend --reuse-message=HEAD")
     (magit-refresh)))
 
-;; full screen magit-status
-(defadvice magit-status (around magit-fullscreen activate)
-  (window-configuration-to-register :magit-fullscreen)
-  ad-do-it
-  (delete-other-windows))
-
 ;; quit by pressing q
 (defun magit-quit-session ()
   "Restores the previous window configuration and kills the magit buffer"
   (interactive)
-  (kill-buffer)
-  (jump-to-register :magit-fullscreen))
+  (kill-buffer))
 
+;; magit full screen
+(setq magit-status-buffer-switch-function
+      (lambda (buffer) ; there might already be an Emacs function which does this
+        (pop-to-buffer buffer)
+        (delete-other-windows)))
+
+;; Don't ask for pushes
 (setq magit-push-always-verify nil)
