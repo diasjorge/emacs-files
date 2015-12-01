d8bf526a26f409529682c19037626539f2134d49
Jorge Dias <jorge@mrdias.com>
2015-12-01 23:16:34 +0100

    Autoload virtual env

--- lisp/el-get-init-files/init-virtualenvwrapper.el
+++ lisp/el-get-init-files/init-virtualenvwrapper.el
@@ -1,7 +1,16 @@
 (venv-initialize-interactive-shells) ;; if you want interactive shell support
 (venv-initialize-eshell)
 
-(add-hook 'python-mode-hook (lambda ()
-                              (hack-local-variables)
-                              (when (boundp 'project-venv-name)
-                                (venv-workon project-venv-name))))
+(defun activate-corresponding-virtual-env ()
+  (interactive)
+  (require 'fiplr)
+  (let ((dirname (file-name-nondirectory (fiplr-root))))
+    (if (venv-is-valid dirname)
+        (venv-workon dirname))))
+
+(add-hook 'python-mode-hook
+          (lambda ()
+            (hack-local-variables)
+            (if (boundp 'project-venv-name)
+                (venv-workon project-venv-name)
+              (activate-corresponding-virtual-env))))
