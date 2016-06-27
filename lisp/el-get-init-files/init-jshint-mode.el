5c4d2e8df4c1c7bd5146bd96cb6d8e4cf34dd100
Jorge Dias <jorge.dias@schibsted.com>
2016-06-27 16:51:05 +0200

    Don't run jshint allways on js2-mode

--- lisp/el-get-init-files/init-jshint-mode.el
+++
@@ -1,7 +0,0 @@
-(add-hook 'js2-mode-hook
-          (lambda ()
-            (add-hook 'after-save-hook
-                      (lambda ()
-                        (require 'flymake-jshint)
-                        (flymake-start-syntax-check)
-                        nil 'make-it-local))))
