3d0fc31cc1a1490b16acac4c0181cd095f30b392
Jorge Dias <jorge.dias@schibsted.com>
2017-07-26 11:36:47 +0200

    Remove hardcoded offset for web-mode

--- lisp/el-get-init-files/init-web-mode.el
+++ lisp/el-get-init-files/init-web-mode.el
@@ -1,8 +1,2 @@
-(add-hook 'web-mode-hook
-          (lambda ()
-            (setq web-mode-markup-indent-offset 2)
-            (setq web-mode-code-indent-offset 2)
-            ))
-
 (add-to-list 'auto-mode-alist '("\\.erb$" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.hbs$" . web-mode))
