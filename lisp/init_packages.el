22d78edd727bdee76b0a72d886f28a70f6a74b5d
Jorge Dias <jorge.dias@schibsted.com>
2017-07-26 16:14:15 +0200

    Fix list of package sources

--- lisp/init_packages.el
+++ lisp/init_packages.el
@@ -1,12 +1,8 @@
 ;; packages
 (require 'package)
 
-(setq package-archives
-      '(("ELPA" . "http://tromey.com/elpa/")
-        ("gnu" . "http://elpa.gnu.org/packages/")
-        ("marmalade" . "http://marmalade-repo.org/packages/")
-        ("SC"   . "http://joseito.republika.pl/sunrise-commander/")
-        ("melpa" . "http://melpa.milkbox.net/packages/")))
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 
 (package-initialize t)
 
