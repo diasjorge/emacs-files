0d6d468c6fc4e60ad4b65d148c4871169146294d
Jorge Dias <jorge.dias@schibsted.com>
2017-11-20 15:27:23 +0100

    Make js2 settings local

--- lisp/init_packages/js.el
+++ lisp/init_packages/js.el
@@ -1,9 +1,9 @@
 (use-package js2-mode
   :mode "\\.js$"
   :config
-  (setq js2-consistent-level-indent-inner-bracket-p t)
-  (setq js2-pretty-multiline-decl-indentation-p t)
-  (setq js2-basic-offset 2)
+  (add-hook 'js2-mode-hook '(lambda ()
+                              (setq-local js2-consistent-level-indent-inner-bracket-p t)
+                              (setq-local js2-pretty-multiline-decl-indentation-p t)))
   (add-hook 'js2-mode-hook 'untabify-hook))
 
 (use-package js2-refactor)
