efd73118391542eba6fd1bd05973115074bed5b5
Jorge Dias <jorge.dias@schibsted.com>
2017-11-20 15:27:51 +0100

    Ruby key bindings

--- lisp/init_packages/ruby.el
+++ lisp/init_packages/ruby.el
@@ -38,9 +38,9 @@
   (add-hook 'robe-mode-hook 'ac-robe-setup)
   (defadvice inf-ruby-console-auto (before activate-rvm-for-robe activate)
     (rvm-activate-corresponding-ruby))
-  (add-hook 'robe-mode-hook '(lambda ()
-                               (local-set-key (kbd "M-.") 'robe-jump)
-                               (local-set-key (kbd "M->") 'robe-doc))))
+  :bind (:map robe-mode-map
+              ("M-." . robe-jump)
+              ("M->" . robe-doc)))
 
 (use-package ruby-compilation)
 
