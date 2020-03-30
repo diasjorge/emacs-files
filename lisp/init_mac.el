e401eb6bcb29b07ddc590668183a1f3f0a3417ec
Jorge Dias <jorge@mrdias.com>
2020-03-30 11:51:50 +0200

    Default font works with daemon mode too

--- lisp/init_mac.el
+++ lisp/init_mac.el
@@ -1,6 +1,6 @@
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
-(set-default-font "Monaco-15" t t)
+(add-to-list 'default-frame-alist '(font . "Monaco-15"))
 (defvar javascript-mode-syntax-table 'js-mode-syntax-table)
 (menu-bar-mode)
 (setq dired-use-ls-dired nil)
