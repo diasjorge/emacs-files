818963e8f03be2de836bba1d180f8dac08ad1c6d
Jorge Dias <jorge@mrdias.com>
2024-03-12 13:46:38 -0400

    Fixes for python mode

--- lisp/init_packages/python.el
+++ lisp/init_packages/python.el
@@ -1,8 +1,3 @@
-(use-package jedi
-  :config
-  (add-hook 'python-mode-hook 'jedi:setup)
-  (setq jedi:complete-on-dot t))
-
 (use-package jinja2-mode
   :mode "\\.j2\\'")
 
@@ -16,37 +11,45 @@
          ("C-x t" . elpy-test-file)
          ("C-x T" . elpy-test-project))
   :defer t
-  :init
-  (advice-add 'python-mode :before 'elpy-enable)
+  :hook  ((python-mode . elpy-enable)
+          (elpy-mode . (lambda () (add-hook 'before-save-hook 'elpy-format-code nil 'local))))
   :custom
-  (elpy-modules '(elpy-module-eldoc elpy-module-flymake elpy-module-highlight-indentation elpy-module-yasnippet elpy-module-sane-defaults))
+  (elpy-modules '(elpy-module-eldoc
+                  elpy-module-flymake
+                  elpy-module-highlight-indentation
+                  elpy-module-yasnippet
+                  elpy-module-sane-defaults))
   :config
   (defun elpy-test-project()
+    "Run tests for the entire project."
     (interactive)
     (funcall elpy-test-runner (elpy-project-root) nil nil nil))
   (defun elpy-test-file()
+    "Run tests for the current file."
     (interactive)
     (let* ((top (elpy-library-root))
            (file buffer-file-name)
            (module (elpy-test--module-name-for-file top file)))
       (funcall elpy-test-runner top file module nil))))
 
-(defun activate-corresponding-virtual-env ()
+(use-package python
+  :ensure nil ; python-mode is typically bundled with Emacs
+  :hook (python-mode . my-python-mode-hook))
+
+(defun my-activate-corresponding-virtual-env ()
   (interactive)
-  (require 'fiplr)
-  (let ((dirname (file-name-nondirectory (directory-file-name (fiplr-root)))))
-    (if (venv-is-valid dirname)
+  (let ((dirname (file-name-nondirectory (directory-file-name (projectile-project-root)))))
+    (if (and (file-exists-p venv-location)
+             (venv-is-valid dirname))
         (progn
           (venv-workon dirname)
           (pyvenv-workon dirname)))))
 
-(add-hook 'python-mode-hook '(lambda ()
-                               (hack-local-variables)
-                               (if (boundp 'project-venv-name)
-                                   (progn
-                                     (venv-workon project-venv-name)
-                                     (pyvenv-workon project-venv-name))
-                                 (activate-corresponding-virtual-env))))
-
-(add-hook 'elpy-mode-hook
-          (lambda () (add-hook 'before-save-hook 'elpy-format-code nil 'local)))
+(defun my-python-mode-hook ()
+  "Custom `python-mode' hook."
+  (hack-local-variables)
+  (if (boundp 'project-venv-name)
+      (progn
+        (venv-workon project-venv-name)
+        (pyvenv-workon project-venv-name))
+    (my-activate-corresponding-virtual-env)))
