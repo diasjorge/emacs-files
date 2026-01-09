dc48e134669198e76851b013b1ceb32f0124ed39
Jorge Dias <jorge@mrdias.com>
2026-01-09 15:14:23 -0400

    Various

--- lisp/init_packages/js.el
+++ lisp/init_packages/js.el
@@ -4,17 +4,16 @@
          ("\\.cjs\\'" . js-ts-mode)
          ("\\.mjs\\'" . js-ts-mode)
          ("\\.jsx\\'" . js-ts-mode)
-         ("\\.json\\'" . js-ts-mode)
-         )
+         ("\\.json\\'" . json-ts-mode))
   :init
   (setq js-indent-level 2)
   (setq js-indent-first-init "dynamic")
-  :hook ((js-ts-mode-hook . untabify-hook)
-         ))
+  :hook ((js-ts-mode-hook . untabify-hook)))
 
 (use-package typescript-ts-mode
   :ensure nil
   :mode (("\\.tsx\\'" . tsx-ts-mode)
          ("\\.ts\\'"  . typescript-ts-mode))
   :config
-  (add-hook 'typescript-mode-hook 'untabify-hook))
+  (add-hook 'typescript-mode-hook 'untabify-hook)
+  )
--- lisp/init_packages/prog.el
+++ lisp/init_packages/prog.el
@@ -1,8 +1,7 @@
 (use-package copilot
-  :quelpa (copilot :fetcher github
-                   :repo "copilot-emacs/copilot.el"
-                   :branch "main"
-                   :files ("dist" "*.el"))
+  :vc (:url "https://github.com/copilot-emacs/copilot.el"
+            :rev :newest
+            :branch "main")
   :config
   (add-to-list 'copilot-indentation-alist '(emacs-lisp-mode 4))
   :hook (prog-mode . copilot-mode)
@@ -58,10 +57,6 @@
              ;; (c-or-c++-mode . c-or-c++-ts-mode)
              ;; (css-mode . css-ts-mode)
              ;; (css-mode . css-ts-mode)
-             ;; (js-json-mode . json-ts-mode)
-             ;; (js-mode . typescript-ts-mode)
-             ;; (js2-mode . typescript-ts-mode)
-             ;; (json-mode . json-ts-mode)
              ;; (python-mode . python-ts-mode)
              ;; (ruby-mode . ruby-ts-mode)))
              ;; (sh-base-mode . bash-ts-mode)
@@ -70,3 +65,83 @@
     (add-to-list 'major-mode-remap-alist mapping))
   :config
   (os/setup-install-grammars))
+
+
+(use-package lsp-mode
+  :diminish "LSP"
+  :ensure t
+  :hook ((lsp-mode . lsp-diagnostics-mode)
+         (lsp-mode . lsp-enable-which-key-integration)
+         ((tsx-ts-mode typescript-ts-mode js-ts-mode) . lsp-deferred))
+  :custom
+  (lsp-keymap-prefix "C-c l")           ; Prefix for LSP actions
+  ;; (lsp-completion-provider :none)       ; Using Corfu as the provider
+  ;; (lsp-diagnostics-provider :flycheck)
+  ;; (lsp-session-file (locate-user-emacs-file ".lsp-session"))
+  ;; (lsp-log-io nil)                      ; IMPORTANT! Use only for debugging! Drastically affects performance
+  ;; (lsp-keep-workspace-alive nil)        ; Close LSP server if all project buffers are closed
+  ;; (lsp-idle-delay 0.5)                  ; Debounce timer for `after-change-function'
+  ;; ;; core
+  ;; (lsp-enable-xref t)                   ; Use xref to find references
+  ;; (lsp-auto-configure t)                ; Used to decide between current active servers
+  ;; (lsp-eldoc-enable-hover t)            ; Display signature information in the echo area
+  ;; (lsp-enable-dap-auto-configure t)     ; Debug support
+  ;; (lsp-enable-file-watchers nil)
+  ;; (lsp-enable-folding nil)              ; I disable folding since I use origami
+  ;; (lsp-enable-imenu t)
+  ;; (lsp-enable-indentation nil)          ; I use prettier
+  ;; (lsp-enable-links nil)                ; No need since we have `browse-url'
+  ;; (lsp-enable-on-type-formatting nil)   ; Prettier handles this
+  ;; (lsp-enable-suggest-server-download t) ; Useful prompt to download LSP providers
+  ;; (lsp-enable-symbol-highlighting t)     ; Shows usages of symbol at point in the current buffer
+  ;; (lsp-enable-text-document-color nil)   ; This is Treesitter's job
+
+  ;; (lsp-ui-sideline-show-hover nil)      ; Sideline used only for diagnostics
+  ;; (lsp-ui-sideline-diagnostic-max-lines 20) ; 20 lines since typescript errors can be quite big
+  ;; ;; completion
+  ;; (lsp-completion-enable t)
+  ;; (lsp-completion-enable-additional-text-edit t) ; Ex: auto-insert an import for a completion candidate
+  ;; (lsp-enable-snippet t)                         ; Important to provide full JSX completion
+  ;; (lsp-completion-show-kind t)                   ; Optional
+  ;; ;; headerline
+  ;; (lsp-headerline-breadcrumb-enable t)  ; Optional, I like the breadcrumbs
+  ;; (lsp-headerline-breadcrumb-enable-diagnostics nil) ; Don't make them red, too noisy
+  ;; (lsp-headerline-breadcrumb-enable-symbol-numbers nil)
+  ;; (lsp-headerline-breadcrumb-icons-enable nil)
+  ;; ;; modeline
+  ;; (lsp-modeline-code-actions-enable nil) ; Modeline should be relatively clean
+  ;; (lsp-modeline-diagnostics-enable nil)  ; Already supported through `flycheck'
+  ;; (lsp-modeline-workspace-status-enable nil) ; Modeline displays "LSP" when lsp-mode is enabled
+  ;; (lsp-signature-doc-lines 1)                ; Don't raise the echo area. It's distracting
+  ;; (lsp-ui-doc-use-childframe t)              ; Show docs for symbol at point
+  ;; (lsp-eldoc-render-all nil)            ; This would be very useful if it would respect `lsp-signature-doc-lines', currently it's distracting
+  ;; ;; lens
+  ;; (lsp-lens-enable nil)                 ; Optional, I don't need it
+  ;; ;; semantic
+  ;; (lsp-semantic-tokens-enable nil)      ; Related to highlighting, and we defer to treesitter
+
+  :init
+  (setq lsp-use-plists t))
+
+;; (use-package lsp-completion
+;;   :no-require
+;;   :hook ((lsp-mode . lsp-completion-mode)))
+
+(use-package lsp-ui
+  :ensure t
+  :commands
+  (lsp-ui-doc-show
+   lsp-ui-doc-glance)
+  :bind (:map lsp-mode-map
+              ("C-c C-d" . 'lsp-ui-doc-glance))
+  :after (lsp-mode)
+  :config (setq lsp-ui-doc-show-with-cursor nil      ; Don't show doc when cursor is over symbol - too distracting
+                lsp-ui-doc-include-signature t       ; Show signature
+                lsp-ui-doc-position 'at-point))
+
+
+(use-package combobulate
+  :vc ( :url "https://github.com/mickeynp/combobulate" :rev :newest)
+  :custom
+  (combobulate-key-prefix "C-c o")
+  :hook ((prog-mode . combobulate-mode)))
