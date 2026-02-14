fc81456c067b70b0cd9ea92ada8750a718bd95ea
Jorge Dias <jorge@mrdias.com>
2026-02-14 16:27:19 -0400

    Clean up web.el, remove emmet, use :custom for css

--- lisp/init_packages/web.el
+++ lisp/init_packages/web.el
@@ -1,18 +1,8 @@
 (use-package css-mode
   :mode "\\.scss\\'"
-  :config
-  (add-hook 'css-mode-hook '(lambda ()
-                              (setq css-indent-level 2)
-                              (setq css-indent-offset 2))))
+  :custom
+  (css-indent-offset 2))
 
-(use-package emmet-mode
-  :config
-  ;; Auto-start on any markup modes
-  (add-hook 'sgml-mode-hook 'emmet-mode)
-  ;; enable Emmet's css abbreviation.
-  (add-hook 'css-mode-hook  'emmet-mode)
-  ;; enable Emmet's on web mode
-  (add-hook 'web-mode-hook  'emmet-mode))
 
 (use-package sass-mode)
 
