b358bd10ab2d6d762020b85e3c8ad449e840a551
Jorge Dias <jorge@mrdias.com>
2026-02-14 16:27:12 -0400

    Remove stale mac workarounds

--- lisp/init_mac.el
+++ lisp/init_mac.el
@@ -1,10 +1,5 @@
 (setq mac-command-modifier 'meta)
 (setq ns-function-modifier 'hyper)
 (add-to-list 'default-frame-alist '(font . "Monaco-15"))
-(defvar javascript-mode-syntax-table 'js-mode-syntax-table)
 (menu-bar-mode)
 (setq dired-use-ls-dired nil)
-(setq gnutls-trustfiles '("/usr/local/etc/openssl/cert.pem"))
-
-;; Maximize when opening
-(set-frame-parameter nil 'fullscreen 'maximized)
