6687af7ebbfe1d4e0da12209d3d79438dc0b209e
Jorge Dias <jorge@mrdias.com>
2026-02-14 16:30:36 -0400

    Remove manual use-package bootstrap

--- lisp/init_packages.el
+++ lisp/init_packages.el
@@ -1,17 +1,8 @@
-;; packages
 (require 'package)
 (setq package-enable-at-startup nil)
 (add-to-list 'package-archives
              '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
-
-;; Bootstrap `use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-(eval-when-compile
-  (require 'use-package))
 
 (setq use-package-compute-statistics t)
 
