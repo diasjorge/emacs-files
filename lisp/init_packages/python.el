4f8fd4aec66a17f75e253a755b53247a709520f3
Jorge Dias <jorge@mrdias.com>
2026-05-21 08:22:20 -0400

    Rewrite Python config: python-ts-mode + pyright via lsp-pyright
    Replace virtualenvwrapper/elpy setup with python-ts-mode, lsp-pyright,
    and pyvenv. Disable pylsp, ruff, and semgrep-ls as competing LSP clients.

--- lisp/init_packages/python.el
+++ lisp/init_packages/python.el
@@ -1,29 +1,17 @@
 (use-package jinja2-mode
   :mode "\\.j2\\'")
 
-(use-package virtualenvwrapper
-  :config
-  (venv-initialize-interactive-shells) ;; if you want interactive shell support
-  (venv-initialize-eshell))
+(use-package lsp-pyright
+  :custom
+  (lsp-disabled-clients '(semgrep-ls ruff pylsp))
+  :hook (python-ts-mode . (lambda ()
+                            (require 'lsp-pyright)
+                            (lsp-deferred))))
 
 (use-package python
-  :ensure nil ; python-mode is typically bundled with Emacs
-  :hook (python-mode . my-python-mode-hook))
+  :ensure nil
+  :mode ("\\.py\\'" . python-ts-mode)
+  :interpreter ("python3?" . python-ts-mode))
 
-(defun my-activate-corresponding-virtual-env ()
-  (interactive)
-  (let ((dirname (file-name-nondirectory (directory-file-name (projectile-project-root)))))
-    (if (and (file-exists-p venv-location)
-             (venv-is-valid dirname))
-        (progn
-          (venv-workon dirname)
-          (pyvenv-workon dirname)))))
-
-(defun my-python-mode-hook ()
-  "Custom `python-mode' hook."
-  (hack-local-variables)
-  (if (boundp 'project-venv-name)
-      (progn
-        (venv-workon project-venv-name)
-        (pyvenv-workon project-venv-name))
-    (my-activate-corresponding-virtual-env)))
+(use-package pyvenv
+  :hook (python-ts-mode . pyvenv-mode))
